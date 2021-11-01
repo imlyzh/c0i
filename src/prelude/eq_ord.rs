0e733af14faf1fae263c5ee54a7428d49afb76fe
imlyzh <enterindex@gmail.com>
2021-11-01 16:40:25 +0800

    update native interface

---
+++ src/prelude/eq_ord.rs
@@ -0,0 +1,77 @@
+use lazy_static::lazy_static;
+
+use sexpr_ir::gast::{Handle, symbol::Symbol};
+
+use crate::impl_wrap;
+use crate::value::Value;
+use crate::value::callable::NativeFunction;
+use crate::value::result::{CResult, CError};
+
+use super::LOCATION;
+
+
+fn eq(args: Vec<Value>) -> CResult {
+    if args.len() != 2 {
+        return Err(CError::PrarmsIsNotMatching(2, args.len()));
+    }
+    let a = args.get(0).unwrap();
+    let b = args.get(1).unwrap();
+    Ok(Value::Bool(a == b))
+}
+
+impl_wrap!(EQ_WRAP, EQ_NAME, eq, "eq?", &LOCATION);
+
+fn ne(args: Vec<Value>) -> CResult {
+    if args.len() != 2 {
+        return Err(CError::PrarmsIsNotMatching(2, args.len()));
+    }
+    let a = args.get(0).unwrap();
+    let b = args.get(1).unwrap();
+    Ok(Value::Bool(a != b))
+}
+
+impl_wrap!(NE_WRAP, NE_NAME, ne, "ne?", &LOCATION);
+
+fn lt(args: Vec<Value>) -> CResult {
+    if args.len() != 2 {
+        return Err(CError::PrarmsIsNotMatching(2, args.len()));
+    }
+    let a = args.get(0).unwrap();
+    let b = args.get(1).unwrap();
+    Ok(Value::Bool(a < b))
+}
+
+impl_wrap!(LT_WRAP, LT_NAME, lt, "raw<", &LOCATION);
+
+fn gt(args: Vec<Value>) -> CResult {
+    if args.len() != 2 {
+        return Err(CError::PrarmsIsNotMatching(2, args.len()));
+    }
+    let a = args.get(0).unwrap();
+    let b = args.get(1).unwrap();
+    Ok(Value::Bool(a > b))
+}
+
+impl_wrap!(GT_WRAP, GT_NAME, gt, "raw>", &LOCATION);
+
+fn le(args: Vec<Value>) -> CResult {
+    if args.len() != 2 {
+        return Err(CError::PrarmsIsNotMatching(2, args.len()));
+    }
+    let a = args.get(0).unwrap();
+    let b = args.get(1).unwrap();
+    Ok(Value::Bool(a <= b))
+}
+
+impl_wrap!(LE_WRAP, LE_NAME, le, "raw<=", &LOCATION);
+
+fn ge(args: Vec<Value>) -> CResult {
+    if args.len() != 2 {
+        return Err(CError::PrarmsIsNotMatching(2, args.len()));
+    }
+    let a = args.get(0).unwrap();
+    let b = args.get(1).unwrap();
+    Ok(Value::Bool(a >= b))
+}
+
+impl_wrap!(GE_WRAP, GE_NAME, ge, "raw>=", &LOCATION);
