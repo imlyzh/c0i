3408f49f35f3df30db6b78ff2ec805c890d83bc5
chuigda <icey@icey.tech>
2022-03-25 14:09:33 +0800

    Pause

--- src/eval47/min_scope_analysis.rs
+++ src/eval47/min_scope_analysis.rs
@@ -1,13 +1,15 @@
+use std::borrow::Borrow;
 use std::collections::HashMap;
 use std::marker::PhantomData;
 
 use pr47::data::tyck::TyckInfoPool;
 use sexpr_ir::gast::Handle;
 
-use crate::ast::{Function, TopLevel};
+use crate::ast::{Expr, Function, TopLevel};
 use crate::eval47::commons::{FFIAsyncFunction, FFIFunction, Signature};
 use crate::eval47::data_map::{DataCollection, GValue};
 use crate::eval47::util::{bitcast_usize_i64, clone_signature, MantisGod};
+use crate::value::Value;
 
 pub struct AnalyseContext {
     tyck_info_pool: TyckInfoPool,
@@ -81,26 +83,97 @@ impl AnalyseContext {
 
     fn analyze_function(
         &mut self,
-        _result: &mut AnalyseResult,
+        result: &mut AnalyseResult,
         scope_chain: &mut Option<Box<Scope>>,
         func: Handle<Function>
     ) {
         *scope_chain = Some(Box::new(Scope::new_function_frame(scope_chain.take())));
 
+        let mut var_ids = vec![];
         for param in func.params.iter() {
-            scope_chain.as_mut().unwrap().add_var(param.0.as_str());
+            let var_id = scope_chain.as_mut().unwrap().add_var(param.0.as_str());
+            var_ids.push(bitcast_usize_i64(var_id));
         }
         if let Some(extend_param) = func.extend_params.as_ref() {
-            scope_chain.as_mut().unwrap().add_var(extend_param.0.as_str());
+            let var_id = scope_chain.as_mut().unwrap().add_var(extend_param.0.as_str());
+            var_ids.push(bitcast_usize_i64(var_id));
+        }
+
+        result.data_collection.insert(func.as_ref(), "ParamVarIDs", var_ids);
+
+        for stmt in func.body.iter() {
+            self.analyze_stmt(result, scope_chain, stmt);
         }
 
         *scope_chain = scope_chain.take().unwrap().parent;
+    }
+
+    fn analyze_stmt(
+        &mut self,
+        result: &mut AnalyseResult,
+        scope_chain: &mut Option<Box<Scope>>,
+        stmt: &TopLevel
+    ) {
+        match stmt.borrow() {
+            TopLevel::Function(func_handle) => self.analyze_function(
+                result, scope_chain, func_handle.clone()
+            ),
+            TopLevel::Bind(var_name, expr) => {
+                let var_id = scope_chain.as_mut().unwrap().add_var(var_name.0.as_str());
+                result.data_collection.insert(
+                    stmt,
+                    "VarID",
+                    bitcast_usize_i64(var_id)
+                );
+                self.analyze_expr(result, scope_chain, expr);
+            }
+            TopLevel::Expr(expr) => self.analyze_expr(result, scope_chain, expr)
+        }
+    }
+
+    fn analyze_expr(
+        &mut self,
+        result: &mut AnalyseResult,
+        scope_chain: &mut Option<Box<Scope>>,
+        expr: &Expr
+    ) {
+        match expr {
+            Expr::Value(value) => self.analyze_value(result, scope_chain, value),
+            Expr::Variable(_) => {}
+            Expr::Lambda(_) => {}
+            Expr::Let(_) => {}
+            Expr::Set(_) => {}
+            Expr::Cond(_) => {}
+            Expr::FunctionCall(_) => {}
+        }
+    }
+
+    fn analyze_value(
+        &mut self,
+        result: &mut AnalyseResult,
+        scope_chain: &mut Option<Box<Scope>>,
+        value: &Value
+    ) {
+        match value {
+            Value::Uint(_) => panic!("Uint value is not supported by Pr47"),
+            Value::Str(_) => {}
+            Value::Sym(_) => panic!("Sym value is not supported by Pr47"),
+            Value::Pair(pair) => {
+                self.analyze_value(result, scope_chain, &pair.0);
+                self.analyze_value(result, scope_chain, &pair.1);
+            },
+            Value::Dict(_) => panic!("Not supported yet"),
+            Value::Vec(_) => panic!("Not supported yet"),
+            Value::Callable(_) => unreachable!(),
+            _ => {}
+        }
     }
 }
 
 pub struct AnalyseResult<'a> {
     pub data_collection: DataCollection,
     pub global_data_map: HashMap<String, GValue>,
+    pub global_consts: Vec<GValue>,
     pub ffi_function_in_use: HashMap<String, (FFIFunction, Signature, usize)>,
     pub async_ffi_function_in_use: HashMap<String, (FFIAsyncFunction, Signature, usize)>,
     _phantom: PhantomData<&'a ()>
@@ -111,6 +184,7 @@ impl<'a> AnalyseResult<'a> {
         Self {
             data_collection: DataCollection::new(),
             global_data_map: HashMap::new(),
+            global_consts: Vec::new(),
             ffi_function_in_use: HashMap::new(),
             async_ffi_function_in_use: HashMap::new(),
             _phantom: PhantomData
