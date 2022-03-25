7f502002ac86a8c384dd36f1655e5ecb9594e852
chuigda <icey@icey.tech>
2022-03-25 11:19:15 +0800

    要是能重来 我要选李白

--- src/eval47/data_map.rs
+++ src/eval47/data_map.rs
@@ -1,4 +1,3 @@
-use std::borrow::Borrow;
 use std::collections::HashMap;
 use xjbutil::value::Value;
 
@@ -14,30 +13,30 @@ impl DataCollection {
 
     pub fn insert<T>(
         &mut self,
-        ptr: impl Borrow<T>,
+        ptr: &T,
         key: impl Into<String>,
         value: impl Into<GValue>
     ) {
-        self.0.entry(ptr.borrow() as *const _ as usize)
+        self.0.entry(ptr as *const _ as usize)
             .or_insert_with(DataMap::new)
             .insert(key.into(), value.into());
     }
 
     pub fn get<T>(
         &self,
-        ptr: impl Borrow<T>,
+        ptr: &T,
         key: impl Into<String>
     ) -> Option<&GValue> {
-        self.0.get(&(ptr.borrow() as *const _ as usize))
+        self.0.get(&(ptr as *const _ as usize))
             .and_then(|map| map.get(&key.into()))
     }
 
     pub fn get_mut<T>(
         &mut self,
-        ptr: impl Borrow<T>,
+        ptr: &T,
         key: impl Into<String>
     ) -> Option<&mut GValue> {
-        self.0.get_mut(&(ptr.borrow() as *const _ as usize))
+        self.0.get_mut(&(ptr as *const _ as usize))
             .and_then(|map| map.get_mut(&key.into()))
     }
 }
--- src/eval47/min_scope_analysis.rs
+++ src/eval47/min_scope_analysis.rs
@@ -1,30 +1,107 @@
 use std::collections::HashMap;
 use std::marker::PhantomData;
 
-use crate::ast::TopLevel;
+use pr47::data::tyck::TyckInfoPool;
+use sexpr_ir::gast::Handle;
+
+use crate::ast::{Function, TopLevel};
 use crate::eval47::commons::{FFIAsyncFunction, FFIFunction, Signature};
 use crate::eval47::data_map::{DataCollection, GValue};
-use crate::eval47::util::{clone_signature, MantisGod};
+use crate::eval47::util::{bitcast_usize_i64, clone_signature, MantisGod};
 
 pub struct AnalyseContext {
+    tyck_info_pool: TyckInfoPool,
     ffi_functions: HashMap<String, (FFIFunction, Signature)>,
     async_ffi_functions: HashMap<String, (FFIAsyncFunction, Signature)>,
-
-    result: AnalyseResult,
-    scope_chain: Option<Box<Scope>>
 }
 
-pub struct AnalyseResult {
+impl AnalyseContext {
+    pub fn new() -> Self {
+        AnalyseContext {
+            tyck_info_pool: TyckInfoPool::new(),
+            ffi_functions: HashMap::new(),
+            async_ffi_functions: HashMap::new()
+        }
+    }
+
+    pub fn register_ffi(&mut self, name: impl Into<String>, ffi: FFIFunction) {
+        let signature: Signature = ffi.signature(&mut self.tyck_info_pool);
+        self.ffi_functions.insert(name.into(), (ffi, signature));
+    }
+
+    pub fn register_async_ffi(&mut self, name: impl Into<String>, ffi: FFIAsyncFunction) {
+        let signature: Signature = ffi.signature(&mut self.tyck_info_pool);
+        self.async_ffi_functions.insert(name.into(), (ffi, signature));
+    }
+
+    pub fn min_scope_analyse(&mut self, ast: &[TopLevel]) -> AnalyseResult {
+        let mut result = AnalyseResult::new();
+        let mut scope_chain = Some(Box::new(Scope::new(None)));
+
+        self.analyze_global(&mut result, &mut scope_chain, ast);
+
+        result
+    }
+}
+
+impl AnalyseContext {
+    fn analyze_global(
+        &mut self,
+        result: &mut AnalyseResult,
+        scope_chain: &mut Option<Box<Scope>>,
+        globals: &[TopLevel]
+    ) {
+        for global in globals {
+            if let TopLevel::Function(func_handle) = global {
+                let func_id = scope_chain.as_mut().unwrap().add_func(
+                    func_handle.name
+                        .as_ref()
+                        .expect("Why the function name is empty anyway?")
+                        .0
+                        .as_str()
+                );
+                result.data_collection.insert(
+                    func_handle.as_ref(),
+                    "FunctionID",
+                    bitcast_usize_i64(func_id)
+                );
+            } else {
+                panic!("global variables or expressions are not supported by Pr47");
+            }
+        }
+
+        for global in globals {
+            if let TopLevel::Function(func_handle) = global {
+                self.analyze_function(result, scope_chain, func_handle.clone())
+            } else {
+                unreachable!();
+            }
+        }
+    }
+
+    fn analyze_function(
+        &mut self,
+        _result: &mut AnalyseResult,
+        scope_chain: &mut Option<Box<Scope>>,
+        _func: Handle<Function>
+    ) {
+        *scope_chain = Some(Box::new(Scope::new_function_frame(scope_chain.take())));
+
+        *scope_chain = scope_chain.take().unwrap().parent;
+    }
+}
+
+pub struct AnalyseResult<'a> {
     pub data_collection: DataCollection,
     pub global_data_map: HashMap<String, GValue>,
     pub ffi_function_in_use: HashMap<String, (FFIFunction, Signature, usize)>,
     pub async_ffi_function_in_use: HashMap<String, (FFIAsyncFunction, Signature, usize)>,
-    _phantom: PhantomData<()>
+    _phantom: PhantomData<&'a ()>
 }
 
-impl AnalyseResult {
-    fn new() -> AnalyseResult {
-        AnalyseResult {
+impl<'a> AnalyseResult<'a> {
+    fn new() -> Self {
+        Self {
             data_collection: DataCollection::new(),
             global_data_map: HashMap::new(),
             ffi_function_in_use: HashMap::new(),
@@ -35,6 +112,7 @@ impl AnalyseResult {
 }
 
 struct FunctionFrame {
+    register_alloc: usize,
     captures: HashMap<String, (
         usize, // capture id
         bool, // the captured item is another capture?
@@ -50,7 +128,8 @@ struct Scope {
         bool, // is capture?
         usize // item id
     )>,
-    functions: HashMap<String, usize>
+    functions: HashMap<String, usize>,
+    function_counter: usize
 }
 
 type LookupResult = MantisGod<
@@ -62,7 +141,7 @@ type LookupResult = MantisGod<
 type LookupContext<'a> = (
     &'a HashMap<String, (FFIFunction, Signature)>,
     &'a HashMap<String, (FFIAsyncFunction, Signature)>,
-    &'a mut AnalyseResult
+    &'a mut AnalyseResult<'a>
 );
 
 impl Scope {
@@ -71,7 +150,8 @@ impl Scope {
             parent,
             function_frame: None,
             variables: HashMap::new(),
-            functions: HashMap::new()
+            functions: HashMap::new(),
+            function_counter: 0
         }
     }
 
@@ -79,10 +159,12 @@ impl Scope {
         Self {
             parent,
             function_frame: Some(FunctionFrame {
+                register_alloc: 0,
                 captures: HashMap::new()
             }),
             variables: HashMap::new(),
-            functions: HashMap::new()
+            functions: HashMap::new(),
+            function_counter: 0
         }
     }
 
@@ -147,6 +229,40 @@ impl Scope {
             result
         } else {
             None
+        }
+    }
+
+    fn add_var(&mut self, name: &str) -> usize {
+        let var_id = self.allocate_reg();
+        self.variables.insert(name.to_string(), (false, var_id));
+        var_id
+    }
+
+    fn add_func(&mut self, name: &str) -> usize {
+        let func_id = self.allocate_func();
+        self.functions.insert(name.to_string(), func_id);
+        func_id
+    }
+
+    fn allocate_reg(&mut self) -> usize {
+        if let Some(function_frame) = self.function_frame.as_mut() {
+            let reg = function_frame.register_alloc;
+            function_frame.register_alloc += 1;
+            reg
+        } else {
+            self.parent.as_mut()
+                .expect("there must be a function frame")
+                .allocate_reg()
+        }
+    }
+
+    fn allocate_func(&mut self) -> usize {
+        if let Some(parent) = self.parent.as_mut() {
+            parent.allocate_func()
+        } else {
+            let func = self.function_counter;
+            self.function_counter += 1;
+            func
         }
     }
 }
